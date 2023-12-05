e54a85215758426813cf33e9953e73e02be2aff2
Dylan Neff <Dyn0402@hotmail.com>
2023-12-05 21:43:35 +0100

    Extensive plotting of the 3 events

--- main.py
+++ main.py
@@ -19,22 +19,39 @@ import ROOT
 import uproot
 import awkward as ak
 
-# matplotlib.use('Qt5Agg')
+# Suppress the specified warning messages
+# ROOT.gErrorIgnoreLevel = ROOT.kError + ROOT.kBreak
+ROOT.gErrorIgnoreLevel = -1
 
 
 def main():
     fdf_dir = 'test_data/fdf/'
     raw_root_dir = 'test_data/raw_root/'
-    files = ['selfTPOTFe_proba_pedthr_230801_17H17_000_05', 'selfTPOTFe_proba_datrun_230801_17H17_000_05']
-    # files = ['selfTPOTFe_proba_pedthr_230801_17H17_000_05']
-    for file in files:
-        raw_root_path = read_fdf_to_root(file, fdf_dir, raw_root_dir)
+    ped_file = 'selfTPOTFe_proba_pedthr_230801_17H17_000_05.root'
+    files = ['selfTPOTFe_proba_datrun_230801_17H17_000_05']
+
+    read_fdfs = False
+    num_detectors = 4
+
+    if read_fdfs:
+        for file in files + [ped_file]:
+            read_fdf_to_root(file, fdf_dir, raw_root_dir)
+
+    ped_root_path = os.path.join(raw_root_dir, ped_file)
+    ped_data = read_det_data(ped_root_path, num_detectors)
+    pedestals = get_pedestals(ped_data)
 
     for file in files:
         raw_root_path = os.path.join(raw_root_dir, file + ".root")
-        # plot_adc_pyroot(raw_root_path)
-        # plot_adc_uproot(raw_root_path)
-        plot_time_samples(raw_root_path)
+        data = read_det_data(raw_root_path, num_detectors)
+        # plot_combined_time_series(data)
+        ped_sub_data = subtract_pedestal(data, pedestals)
+        plot_combined_time_series(ped_sub_data)
+        max_data = get_sample_max(ped_sub_data)
+        plot_position_data(max_data)
+
+    plt.show()
+
     print('donzo')
 
 
@@ -97,34 +114,75 @@ def plot_adc_uproot(file_path):
     plt.show()
 
 
-def plot_time_samples(file_path):
+def read_det_data(file_path, num_detectors=None, variable_name='StripAmpl', tree_name='T'):
     # Open the ROOT file with uproot
     root_file = uproot.open(file_path)
 
     # Access the tree in the file
-    tree = root_file['T']
+    tree = root_file[tree_name]
 
     # Get the variable data from the tree
-    variable_name = 'StripAmpl'
     variable_data = ak.to_numpy(tree[variable_name].array())
-    print(variable_data.shape)
+    root_file.close()
 
-    for det_num, det in enumerate(np.concatenate(variable_data, axis=2)):
-        print(det_num)
-        print(det)
+    if num_detectors is not None:
+        variable_data = variable_data[:, :num_detectors]
+
+    return variable_data
+
+
+def get_pedestals(data):
+    data_event_concat = np.concatenate(data, axis=2)
+    strip_means = np.mean(data_event_concat, axis=2)
+    strip_medians = np.median(data_event_concat, axis=2)
+
+    return strip_medians
+
+
+def plot_combined_time_series(data):
+    n_events, n_samples_per_event = data.shape[0], data.shape[-1]
+    for det_num, det in enumerate(np.concatenate(data, axis=2)):
         fig, ax = plt.subplots()
         for strip in det:
             ax.plot(range(len(strip)), strip)
 
         # Set plot labels and title
+        for event_i in range(n_events):
+            ax.axvline(event_i * n_samples_per_event, color='black', ls='--')
         ax.set_xlabel('Sample Number')
         ax.set_ylabel("ADC")
         ax.set_title(f"Detector #{det_num}")
 
-    # Display the plot
-    plt.show()
+
+def subtract_pedestal(data, pedestal):
+    return data - pedestal[np.newaxis, :, :, np.newaxis]
+
+
+def get_sample_max(data):
+    return np.max(data, axis=-1)
+
+
+def plot_position_data(data):
+    print(data.shape)
+    for event_num, event in enumerate(data):
+        fig, ax = plt.subplots()
+        for det_num, det in enumerate(event):
+            ax.plot(range(len(det)), det, label=f'Detector #{det_num}')
+        ax.set_xlabel('Position')
+        ax.set_ylabel("Max ADC")
+        ax.set_title(f"Event #{event_num}")
+        ax.legend()
+    data_transpose = np.transpose(data, (1, 0, 2))
+    print(data_transpose.shape)
+    for det_num, det in enumerate(data_transpose):
+        fig, ax = plt.subplots()
+        for event_num, event in enumerate(det):
+            ax.plot(range(len(event)), event, label=f'Event #{event_num}')
+        ax.set_xlabel('Position')
+        ax.set_ylabel("Max ADC")
+        ax.set_title(f"Detector #{det_num}")
+        ax.legend()
 
 
 if __name__ == '__main__':
-    # pass
     main()
