3dc01d28f6b6af8453a55afd15c7142f21838253
Dylan Neff <Dyn0402@hotmail.com>
2024-06-30 19:13:42 +0200

    m3_track_quality.py added a function to count events after each cut.
    Realized m3 detector tracking has been bad recently. Need to fix

--- poc/m3_track_quality.py
+++ poc/m3_track_quality.py
@@ -17,8 +17,9 @@ from M3RefTracking import M3RefTracking
 
 
 def main():
-    ray_dir = 'C:/Users/Dylan/Desktop/banco_test3/'
-    m3_ref_tracking = M3RefTracking(ray_dir)
+    # ray_dir = 'C:/Users/Dylan/Desktop/banco_test3/'
+    ray_dir = 'F:/Saclay/banco_data/banco_stats2/'
+    m3_ref_tracking = M3RefTracking(ray_dir, single_track=False)
 
     # numbers = ak.Array(
     #     [
@@ -35,8 +36,48 @@ def main():
     # print(rd['Chi2X'] < 10)
     # print(rd['Chi2X'][rd['Chi2X'] < 10])
     # input()
+    # single_track_test(ray_dir)
     plot_chi2_distributions(m3_ref_tracking.ray_data)
     print('donzo')
+
+
+def single_track_test(ray_dir):
+    m3_ref_tracking = M3RefTracking(ray_dir, single_track=False)
+    chi2_x, chi2_y = m3_ref_tracking.ray_data['Chi2X'], m3_ref_tracking.ray_data['Chi2Y']
+    num_tracks_per_event = ak.num(m3_ref_tracking.ray_data['X_Up'], axis=1)
+    num_events_with_track = ak.sum(num_tracks_per_event > 0)
+    og_total_events = len(m3_ref_tracking.ray_data['X_Up'])
+    print(f'Original total number of events: {og_total_events}')
+    print(f'Original number of events with at least one track: {num_events_with_track}')
+    num_good_tracks = ak.sum((chi2_x < m3_ref_tracking.chi2_cut) & (chi2_y < m3_ref_tracking.chi2_cut), axis=1)
+    num_events_with_good_track = ak.sum(num_good_tracks > 0)
+    print(f'Original number of events with at least one good track: {num_events_with_good_track}')
+    m3_ref_tracking.cut_on_det_size()
+    chi2_x, chi2_y = m3_ref_tracking.ray_data['Chi2X'], m3_ref_tracking.ray_data['Chi2Y']
+    num_good_tracks = ak.sum((chi2_x < m3_ref_tracking.chi2_cut) & (chi2_y < m3_ref_tracking.chi2_cut), axis=1)
+    num_events_with_good_track_chi = ak.sum(num_good_tracks > 0)
+    print(f'Number of events with at least one good track after cut on detector size: {num_events_with_good_track_chi}')
+    num_events_with_single_good_track = ak.sum(num_good_tracks == 1)
+    print(f'Number of events with exactly one good track after cut on detector size: {num_events_with_single_good_track}')
+
+    fig, ax = plt.subplots()
+    # Make horizontal bar graph of the number of events after each of these steps. Write the numbers in bars
+    labels = ['Original', 'Original with Tracks', 'After Chi2 Cut', 'After Detector Size Cut', 'After Single Track Cut']
+    num_events = [og_total_events, num_events_with_track, num_events_with_good_track, num_events_with_good_track_chi,
+                  num_events_with_single_good_track]
+    ax.barh(labels, num_events, height=1)
+    for i, num in enumerate(num_events):
+        ax.text(num / 2, i, str(num), ha='center', va='center', color='w')
+    ax.set_title('Number of Events After Each Cut')
+    fig.tight_layout()
+    plt.show()
+
+    m3_ref_tracking_single = M3RefTracking(ray_dir, single_track=True)
+    print(m3_ref_tracking_single.ray_data['X_Up'])
+    print(m3_ref_tracking.ray_data['X_Up'])
+    print(m3_ref_tracking_single.ray_data)
+    num_events_with_good_track = len(m3_ref_tracking_single.ray_data['X_Up'])
+    print(f'Number of events with at least one good track after single track cut: {num_events_with_good_track}')
 
 
 def plot_chi2_distributions(ray_data):
@@ -59,8 +100,8 @@ def plot_chi2_distributions(ray_data):
         print(chi2_x_ray)
         # print([len(x) for x in chi2_x_ray])
         fig, ax = plt.subplots()
-        ax.hist(chi2_x_ray, bins=100, alpha=0.5, label='Chi2X')
-        ax.hist(chi2_y_ray, bins=100, alpha=0.5, label='Chi2Y')
+        ax.hist(chi2_x_ray, bins=np.arange(-1, max(chi2_x_ray) + 1, 0.5), alpha=0.5, label='Chi2X')
+        ax.hist(chi2_y_ray, bins=np.arange(-1, max(chi2_y_ray) + 1, 0.5), alpha=0.5, label='Chi2Y')
         ax.set_title(f'Ray {ray_num}')
         ax.legend()
     plt.show()
