f02299de0a42ac445d7574ad76f2d216b1d6d70c
Dylan Neff <Dyn0402@hotmail.com>
2025-03-05 23:40:17 +0100

    Tweaks to understand_timestamp.py

--- quick_scripts/understand_timestamp.py
+++ quick_scripts/understand_timestamp.py
@@ -8,6 +8,7 @@ Created as saclay_micromegas/understand_timestamp
 @author: Dylan Neff, dn277127
 """
 
+import platform
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,11 +17,18 @@ import uproot
 
 
 def main():
-    test_root_path = '/local/home/dn277127/Bureau/cosmic_data/ip1_test_2-27-25/long_run_1/filtered_root/'
+    if platform.system() == 'Windows':
+        base_path = 'F:/Saclay/cosmic_data/'
+    elif platform.system() == 'Linux':
+        base_path = '/local/home/dn277127/Bureau/cosmic_data/'
+    else:
+        print('Unknown platform')
+        return
+    test_root_path = f'{base_path}ip1_test_2-27-25/long_run_1/filtered_root/'
 
     event_nums, timestamps, delta_timestamps = [], [], []
     for root_file in os.listdir(test_root_path):
-        if root_file.endswith('.root') and '_04_' in root_file and '_datrun_' in root_file and '_array_' in root_file:
+        if root_file.endswith('.root') and '_06_' in root_file and '_array' in root_file and '_002_' in root_file:
             print(root_file)
             root_path = f'{test_root_path}{root_file}'
             root = uproot.open(root_path)
@@ -44,9 +52,11 @@ def main():
     # Make array of 64 bit binary timestamps
     timestamps_bin = np.array([np.binary_repr(ts, 65) for ts in timestamps])
 
-    for event_i, timestamp_bin_i in zip(event_nums, timestamps_bin):
-        print(f'Event {event_i:5d}:   {timestamp_bin_i}')
-        if (event_i > 1000):
+    min_print_event, max_print_event = 54800, 55100
+    for event_i, timestamp_bin_i, timestamp_i in zip(event_nums, timestamps_bin, timestamps):
+        if min_print_event <= event_i <= max_print_event:
+            print(f'Event {event_i:5d}:   {timestamp_bin_i},  {timestamp_i:e}')
+        if (event_i > max_print_event):
             break
 
     # Plot timestamps vs event_nums
