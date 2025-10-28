8cb80f8931407eb81f57346fb08ab659399b1e5d
Dylan <Dyn0402@hotmail.com>
2025-10-28 15:14:53 +0100

    Added __init__.py to Detector_Classes

---
+++ Detector_Classes/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on October 28 15:14 2025
+Created in PyCharm
+Created as saclay_micromegas/__init__
+
+@author: Dylan Neff, dn277127
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    print('donzo')
+
+
+if __name__ == '__main__':
+    main()
